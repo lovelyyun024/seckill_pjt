bdfba7470ec11669d7118079dd84ced1b7ec626e
Yun Zhang <yzhang4098@gatech.edu>
2024-09-18 13:18:44 -0700

    day4

--- src/test/java/com/jiuzhang/seckill/MQTest.java
+++ src/test/java/com/jiuzhang/seckill/MQTest.java
@@ -1,4 +1,22 @@
 package com.jiuzhang.seckill;
 
+import com.jiuzhang.seckill.mq.RocketMQService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+
+@SpringBootTest
 public class MQTest {
+
+    @Autowired
+    RocketMQService rocketMQService;
+
+    @Test
+    public void sendMQTest() throws Exception {
+        rocketMQService.sendMessage("test-jiuzhang", "Hello World!" + new Date().toString());
+    }
+
+
 }
