1773c945bea58973641e14a76176c0270b7f9f5d
Yun Zhang <yzhang4098@gatech.edu>
2024-09-21 07:35:05 -0700

    finished day5

--- src/main/java/com/jiuzhang/seckill/services/SeckillActivityService.java
+++ src/main/java/com/jiuzhang/seckill/services/SeckillActivityService.java
@@ -3,6 +3,7 @@ package com.jiuzhang.seckill.services;
 import com.alibaba.fastjson.JSON;
 import com.jiuzhang.seckill.db.dao.OrderDao;
 import com.jiuzhang.seckill.db.dao.SeckillActivityDao;
+import com.jiuzhang.seckill.db.dao.SeckillCommodityDao;
 import com.jiuzhang.seckill.db.po.Order;
 import com.jiuzhang.seckill.db.po.SeckillActivity;
 import com.jiuzhang.seckill.mq.RocketMQService;
@@ -26,8 +27,12 @@ public class SeckillActivityService {
 
     @Autowired
     private RocketMQService rocketMQService;
+
     @Autowired
-    private OrderDao orderDao;
+    SeckillCommodityDao seckillCommodityDao;
+
+    @Autowired
+    OrderDao orderDao;
 
     /**
      * datacenterId;  数据中心
@@ -36,17 +41,6 @@ public class SeckillActivityService {
      * 单机开发环境中先写死
      */
     private SnowFlake snowFlake = new SnowFlake(1, 1);
-
-    /**
-     * 判断秒杀库存
-     *
-     * @param activityId
-     * @return
-     */
-    public boolean seckillStockValidator(long activityId) {
-        String key = "stock:" + activityId;
-        return redisService.stockDeductValidator(key);
-    }
 
     /**
      * 创建订单
@@ -83,19 +77,45 @@ public class SeckillActivityService {
     }
 
     /**
-     * 订单支付完成处理
-     * @param orderNo
+     * 判断商品是否还有库存
+     *
+     * @param activityId 商品ID
+     * @return
      */
-    public void payOrderProcess(String orderNo) {
-        log.info("完成支付订单 订单号：" + orderNo);
-        Order order = orderDao.queryOrder(orderNo);
-        boolean deductStockResult = seckillActivityDao.deductStock(order.getSeckillActivityId());
-        if (deductStockResult) {
-            order.setPayTime(new Date());
-            // 订单状态 0、没有可用库存，无效订单  1、已创建等待支付  2、完成支付
-            order.setOrderStatus(2);
-            orderDao.updateOrder(order);
-        }
+    public boolean seckillStockValidator(long activityId) {
+        String key = "stock:" + activityId;
+        return redisService.stockDeductValidator(key);
     }
 
+    /**
+     * 订单支付完成处理
+     *
+     * @param orderNo
+     */
+    public void payOrderProcess(String orderNo) throws Exception {
+        log.info("完成支付订单  订单号：" + orderNo);
+        Order order = orderDao.queryOrder(orderNo);
+        /*
+         * 1.判断订单是否存在
+         * 2.判断订单状态是否为未支付状态
+         */
+        if (order == null) {
+            log.error("订单号对应订单不存在：" + orderNo);
+            return;
+        } else if(order.getOrderStatus() != 1 ) {
+            log.error("订单状态无效：" + orderNo);
+            return;
+        }
+        /*
+         * 2.订单支付完成
+         */
+        order.setPayTime(new Date());
+        //订单状态 0:没有可用库存，无效订单 1:已创建等待付款 ,2:支付完成
+        order.setOrderStatus(2);
+        orderDao.updateOrder(order);
+        /*
+         *3.发送订单付款成功消息
+         */
+        rocketMQService.sendMessage("pay_done", JSON.toJSONString(order));
+    }
 }
