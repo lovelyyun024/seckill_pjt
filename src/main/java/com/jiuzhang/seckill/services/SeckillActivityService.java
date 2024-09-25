cf982d03805c0f2b2ae5a404efd81c9a1c85e71a
Yun Zhang <yzhang4098@gatech.edu>
2024-09-25 16:58:04 -0700

    finished day6

--- src/main/java/com/jiuzhang/seckill/services/SeckillActivityService.java
+++ src/main/java/com/jiuzhang/seckill/services/SeckillActivityService.java
@@ -6,6 +6,7 @@ import com.jiuzhang.seckill.db.dao.SeckillActivityDao;
 import com.jiuzhang.seckill.db.dao.SeckillCommodityDao;
 import com.jiuzhang.seckill.db.po.Order;
 import com.jiuzhang.seckill.db.po.SeckillActivity;
+import com.jiuzhang.seckill.db.po.SeckillCommodity;
 import com.jiuzhang.seckill.mq.RocketMQService;
 import com.jiuzhang.seckill.util.RedisService;
 import com.jiuzhang.seckill.util.SnowFlake;
@@ -85,6 +86,20 @@ public class SeckillActivityService {
     public boolean seckillStockValidator(long activityId) {
         String key = "stock:" + activityId;
         return redisService.stockDeductValidator(key);
+    }
+
+
+    /**
+     * 将秒杀详情相关信息倒入redis
+     *
+     * @param seckillActivityId
+     */
+    public void pushSeckillInfoToRedis(long seckillActivityId) {
+        SeckillActivity seckillActivity = seckillActivityDao.querySeckillActivityById(seckillActivityId);
+        redisService.setValue("seckillActivity:" + seckillActivityId, JSON.toJSONString(seckillActivity));
+
+        SeckillCommodity seckillCommodity = seckillCommodityDao.querySeckillCommodityById(seckillActivity.getCommodityId());
+        redisService.setValue("seckillCommodity:" + seckillActivity.getCommodityId(), JSON.toJSONString(seckillCommodity));
     }
 
     /**
--- src/test/java/com/jiuzhang/seckill/PayOrderTest.java
+++ src/test/java/com/jiuzhang/seckill/PayOrderTest.java
@@ -1,7 +1,6 @@
 package com.jiuzhang.seckill;
 
 import com.jiuzhang.seckill.services.SeckillActivityService;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -11,8 +10,8 @@ public class PayOrderTest {
     @Autowired
     private SeckillActivityService seckillActivityService;
 
-    @Test
-    public void payOrderTest(){
-        seckillActivityService.payOrderProcess("");
-    }
+//    @Test
+//    public void payOrderTest(){
+//        seckillActivityService.payOrderProcess("");
+//    }
 }
